62db73ba8788d9bd28fd70c85c65ddaebb1d8cda
hui <zhanghuizaizhe@hotmail.com>
2016-09-27 22:03:45 +0800

    test work-stealing pool

--- main/fp/concurrent/ScheduleExecutorTest.java
+++ main/fp/concurrent/ScheduleExecutorTest.java
@@ -1,19 +1,76 @@
 package fp.concurrent;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Created by hzhang3 on 9/27/2016.
  */
 public class ScheduleExecutorTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 //        testDelyForScheduleExecutor();
-        testFixedRate();
+//        testFixedRate();
+//        testFixedDely();
+//        testInvokeAny();
+        testInvokeAll();
     }
+
+    static void testInvokeAll() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newWorkStealingPool();
+
+        List<Callable<String>> callables = Arrays.asList( callable("task1",1), callable("task2", 0), callable("task3",0) );
+
+        service.invokeAll(callables)
+                .stream()
+                .map(future -> {
+                    try {
+                        return future.get();
+                    }
+                    catch (Exception e) {
+                        throw new IllegalStateException(e);
+                    }
+                })
+                .forEach(System.out::println);
+        service.shutdown();
+
+    }
+
+    static void testInvokeAny() throws ExecutionException, InterruptedException {
+        ExecutorService service = Executors.newWorkStealingPool();
+
+        List<Callable<String>> callables = Arrays.asList( callable("task1",10), callable("task2", 2 ), callable("task3",3) );
+
+        String result = service.invokeAny(callables);
+        System.out.println(result);
+
+        service.shutdown();
+
+    }
+
+    private static Callable<String> callable(String result, long sleep) {
+        return () -> {
+            TimeUnit.SECONDS.sleep(sleep);
+            return result;
+        };
+    }
+
+    static void testFixedDely(){
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+
+        Runnable task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("scheduling: " + System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                System.err.println("task interrupted");
+            }
+        };
+
+        scheduledExecutorService.scheduleWithFixedDelay(task,0,1,TimeUnit.SECONDS);
+    }
+
 
     static void testFixedRate(){
         ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
